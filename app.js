56c82dcfe146c4b3868d20fa4c9d1e4eba258732
ChiriacIoana <chiriacioana2008@gmail.com>
2025-07-02 15:13:24 +0300

    routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import {PORT} from './config/env.js'; // Import the PORT variable from the environment configuration
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 
 const app = express(); // Create an Express application
+
+app.use('api/v1/auth', authRouter); // Use the auth router for authentication-related routes
+app.use('/api/v1/users', userRouter); // Use the user router for user-related routes
+app.use('/api/v1/subscriptions', subscriptionRouter); // Use the subscription router for subscription-related routes
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscrition Tracker API!');
---
+++ routes/subscription.routes.js
@@ -0,0 +1,45 @@
+import {Router} from 'express';
+
+const subscriptionRouter = Router();
+
+// Define a route to get all subscriptions
+subscriptionRouter.get('/', (req, res) => {
+    res.send({title: 'GET all subscriptions'}); // Placeholder response for fetching all subscriptions
+});
+
+// Define a route to get a specific subscription by ID
+subscriptionRouter.get('/:id', (req, res) => {
+    res.send({title: 'GET subscription details'}); // Placeholder response for fetching all subscriptions
+});
+
+// Define a route to create a new subscription
+subscriptionRouter.post('/', (req, res) => {
+    res.send({title: 'CREATE subscription'}); // Placeholder response for fetching all subscriptions
+});
+
+// Define a route to delete a subscription by ID
+subscriptionRouter.put('/:id', (req, res) => {
+    res.send({title: 'UPDATE subscription'}); // Placeholder response for fetching all subscriptions
+});
+
+// Define a route to delete a subscription by ID
+subscriptionRouter.delete('/:id', (req, res) => {
+    res.send({title: 'DELETE subscription'}); // Placeholder response for fetching all subscriptions
+});
+
+// Define a route to get all subscriptions for a specific user
+subscriptionRouter.get('/user/:id', (req, res) => {
+    res.send({title: 'GET all user subscriptions'}); // Placeholder response for fetching all subscriptions
+});
+
+// Define a route to cancel a subscription by ID
+subscriptionRouter.put('/:id/cancel', (req, res) => {
+    res.send({title: 'CANCEL subscription'}); // Placeholder response for fetching all subscriptions
+});
+
+// Define a route to get upcoming renewals for all subscriptions
+subscriptionRouter.get('/upcoming-renewals', (req, res) => {
+    res.send({title: 'GET upcoming renewals'}); // Placeholder response for fetching all subscriptions
+});
+
+export default subscriptionRouter;
